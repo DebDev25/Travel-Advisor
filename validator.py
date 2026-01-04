fdfd96464fcd449451c81a9a19dd5a0e097e1970
DebDev25 <bdesperado24@gmail.com>
2026-01-04 15:44:59 +0530

    Added API response handling, input validation

---
+++ validator.py
@@ -0,0 +1,10 @@
+from ISO_Codes import country_to_iso
+import re
+
+def valid_country(country: str) -> str:
+    country = country.strip().title()
+    if not re.fullmatch(r"[A-Za-z\s\-]+", country):
+        raise ValueError("Country name contains invalid characters")
+    if country not in country_to_iso:
+        raise ValueError("Country not supported")
+    return country_to_iso[country]
